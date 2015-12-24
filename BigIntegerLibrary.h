68731a1664e32addbab6fd63f9b239403a80917d
root <root@localhost.localdomain>
2015-12-24 10:57:47 +0800

    YYQ:删除相似计算等相关的不必要的文件。

--- BigIntegerLibrary.h
+++
@@ -1,8 +0,0 @@
-// This header file includes all of the library header files.
-
-#include "NumberlikeArray.h"
-#include "BigUnsigned.h"
-#include "BigInteger.h"
-#include "BigIntegerAlgorithms.h"
-#include "BigUnsignedInABase.h"
-#include "BigIntegerUtils.h"
